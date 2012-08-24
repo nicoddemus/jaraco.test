cb2a489fb4bfbda228762f90bc503b1b2677f1fe
Jason R. Coombs <jaraco@jaraco.com>
2012-08-24 11:37:42 -0400

    Add dependency on jaraco.util

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup_params = dict(
 	packages=setuptools.find_packages(),
 	namespace_packages=['jaraco'],
 	zip_safe=False,
+	install_requires=[
+		'jaraco.util',
+	],
 	setup_requires=[
 		'hgtools',
 	],
